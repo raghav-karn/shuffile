c49d6bee34c82692218aa3673dda9c8147676a3f
raghav-karn <raghavkarn@outlook.com>
2024-12-26 22:26:27 +0530

    Scripted constants

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
