e457586f144de1506ef8cdbb4e230a73e3974a57
raghav-karn <raghavkarn@outlook.com>
2025-01-02 21:53:30 +0530

    Scripted receiver button

--- public/receiver.js
+++ public/receiver.js
@@ -7,4 +7,18 @@
 		return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
 	}
 
+	document.querySelector("#receiver-start-con-btn").addEventListener("click",function(){
+		sender_uid = document.querySelector("#join-id").value;
+		if(sender_uid.length == 0){
+			return;
+		}
+		let joinID = generateID();
+		socket.emit("receiver-join", {
+			sender_uid:sender_uid,
+			uid:joinID
+		});
+		document.querySelector(".join-screen").classList.remove("active");
+		document.querySelector(".fs-screen").classList.add("active");
+	});
+
 })();
