0f67a2ce60043dbe00c8e8fb57bf466d88af4cbe
raghav-karn <raghavkarn@outlook.com>
2025-01-02 20:59:38 +0530

    Scripted generateID button

--- public/client.js
+++ public/client.js
@@ -7,4 +7,15 @@
 		return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
 	}
 
+	document.querySelector("#sender-start-con-btn").addEventListener("click",function(){
+		let joinID = generateID();
+		document.querySelector("#join-id").innerHTML = `
+			<b>Room ID</b>
+			<span>${joinID}</span>
+		`;
+		socket.emit("sender-join", {
+			uid:joinID
+		});
+	});
+
 })();
