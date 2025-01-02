6a794ea86efd042adec44b273474711d4a848d6a
raghav-karn <raghavkarn@outlook.com>
2025-01-02 21:46:36 +0530

    Scripted room ID generation

--- public/receiver.js
+++ public/receiver.js
@@ -3,4 +3,8 @@
 	const socket = io();
 	let sender_uid;
 
+	function generateID(){
+		return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
+	}
+
 })();
