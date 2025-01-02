e8172bdf5c87528af5975171a9ceda8d3ab88f77
raghav-karn <raghavkarn@outlook.com>
2025-01-02 21:23:50 +0530

    Scripted file share function

--- public/client.js
+++ public/client.js
@@ -49,4 +49,25 @@
 		reader.readAsArrayBuffer(file);
 	});
 
+	function shareFile(metadata,buffer,progress_node){
+		socket.emit("file-meta", {
+			uid:receiverID,
+			metadata:metadata
+		});
+		
+		socket.on("fs-share",function(){
+			let chunk = buffer.slice(0,metadata.buffer_size);
+			buffer = buffer.slice(metadata.buffer_size,buffer.length);
+			progress_node.innerText = Math.trunc(((metadata.total_buffer_size - buffer.length) / metadata.total_buffer_size * 100));
+			if(chunk.length != 0){
+				socket.emit("file-raw", {
+					uid:receiverID,
+					buffer:chunk
+				});
+			} else {
+				console.log("Sent file successfully");
+			}
+		});
+	}
+
 })();
