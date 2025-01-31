e753bb7ad2a05b24d358c5d7e7d04d85a961dc5f
raghav-karn <raghavkarn@outlook.com>
2025-01-31 13:49:28 +0530

    FInished socket io files sharing

--- public/original/receiver.js
+++ public/original/receiver.js
@@ -46,7 +46,7 @@
 	socket.on("fs-share", function (buffer) {
 		fileShare.buffer.push(buffer);
 		fileShare.transmitted += buffer.byteLength;
-		fileShare.progrss_node.innerText = Math.trunc(fileShare.transmitted / fileShare.metadata.total_buffer_size * 100) + '%';
+		fileShare.progrss_node.innerText = Math.trunc(fileShare.transmitted / fileShare.metadata.total_buffer_size * 100);
 		if (fileShare.transmitted == fileShare.metadata.total_buffer_size) {
 			download(new Blob(fileShare.buffer), fileShare.metadata.filename);
 			fileShare = {};
